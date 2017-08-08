3f58d70c01b1b550d42fccd77f4b4c84c4f2d969
Montel Laurent <montel@kde.org>
2017-08-08 22:12:08 +0200

    Use QOverload

--- src/manual/manualselectfilterpage.cpp
+++ src/manual/manualselectfilterpage.cpp
@@ -57,7 +57,7 @@ ManualSelectFilterPage::ManualSelectFilterPage(QWidget *parent)
     mWidget = new Ui::ManualSelectFilterPage;
     mWidget->setupUi(this);
     mWidget->mIntroSidebar->setPixmap(QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("importwizard/pics/step1.png")));
-    connect(mWidget->mFilterCombo, static_cast<void (KComboBox::*)(int)>(&KComboBox::activated), this, &ManualSelectFilterPage::filterSelected);
+    connect(mWidget->mFilterCombo, QOverload<int>::of(&KComboBox::activated), this, &ManualSelectFilterPage::filterSelected);
 
     addFilter(new MailImporter::FilterKMailArchive);
     addFilter(new MailImporter::FilterMBox);
