ff03a8e264ca1cccd4715e0f3a726651b40bf37b
Laurent Montel <montel@kde.org>
2018-02-16 23:51:47 +0100

    increase version

--- src/importwizard.cpp
+++ src/importwizard.cpp
@@ -204,7 +204,7 @@ void ImportWizard::createAutomaticModePage()
 void ImportWizard::initializeImportModule()
 {
     const QVector<LibImportWizard::AbstractImporter *> lstPlugins = ImportMailPluginManager::self()->pluginsList();
-    for (LibImportWizard::AbstractImporter *abstractPlugin : qAsConst(lstPlugins)) {
+    for (LibImportWizard::AbstractImporter *abstractPlugin : lstPlugins) {
         if (abstractPlugin->foundMailer()) {
             abstractPlugin->setAbstractDisplayInfo(mAbstractDisplayInfo);
             mlistImport.insert(abstractPlugin->name(), abstractPlugin);
