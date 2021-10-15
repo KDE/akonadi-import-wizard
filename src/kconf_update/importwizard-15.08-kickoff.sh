7058a4fba51a9dc5433701cbeb2c3c2e2961c3d7
Laurent Montel <montel@kde.org>
2021-10-15 08:19:09 +0200

    GIT_SILENT: Remove old kconf_update

--- src/kconf_update/importwizard-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/importwizard.desktop/\/org.kde.importwizard.desktop/" $kickoffrcname
-fi
